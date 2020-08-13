75cdbed91ec8d625f7933081463e9f1b91877794
cvzi <cuzi@openmail.cc>
2020-08-13 23:01:44 +0200

    footer

--- GeniusLyrics.js
+++ GeniusLyrics.js
@@ -15,6 +15,22 @@
 // @connect      genius.com
 // ==/UserScript==
 
+/*
+    Copyright (C) 2019 cuzi (cuzi@openmail.cc)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 // TODO save scroll speed global and per song
 // TODO pause button for instrumental parts
 
@@ -1887,6 +1903,12 @@ Genius:  ${originalUrl}
         requestCache = {}
       })
     })
+
+    // Footer
+    div = win.appendChild(document.createElement('div'))
+    div.innerHTML = `<p style="font-size:15px;">
+      Powered by <a style="font-size:15px;" target="_blank" href="https://github.com/cvzi/GeniusLyricsUserscriptLibrary/">GeniusLyrics.js</a>, Copyright © 2019 <a style="font-size:15px;" href="mailto:cuzi@openmail.cc">cuzi</a>.
+      <br>Licensed under the GNU General Public License v3.0</p>`
   }
 
   function addOneMessageListener (type, cb) {
